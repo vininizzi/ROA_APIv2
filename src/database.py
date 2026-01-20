72233ca90a36853cfed672b812f7051d5238359c
Natanco1 <nata.oliveira2004@gmail.com>
2026-01-20 13:24:40 -0300

    feat: organize project's .env

--- src/database.py
+++ src/database.py
@@ -1,9 +1,13 @@
+import os
+from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import Session, sessionmaker
 
+load_dotenv()
+DATABASE_URL = os.getenv("DATABASE_URL")
+
 # database setup
-DATABASE_URL = "sqlite:///./test.db"
 engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
