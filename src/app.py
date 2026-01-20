145e47f60d94d0921fb7816c754072eadd1cf359
Natanco1 <nata.oliveira2004@gmail.com>
2026-01-20 10:57:40 -0300

    database setup

--- src/app.py
+++ src/app.py
@@ -4,12 +4,15 @@ import uvicorn
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
-from sqlalchemy import Column, Integer, String, create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import Session, sessionmaker
 
 from routes import general
+from database import engine, Base
+from models import users
 
+# database setup
+Base.metadata.create_all(bind=engine)
+
+# app setup
 app = FastAPI(title="sql alchemy")
 
 app.add_middleware(
