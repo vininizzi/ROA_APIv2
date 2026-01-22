82f40c57775eb789e622178d5f1751faab4978fe
Natanco1 <nata.oliveira2004@gmail.com>
2026-01-22 10:50:22 -0300

    fix: send more info as venv vars

--- src/core/security.py
+++ src/core/security.py
@@ -12,8 +12,8 @@ from database import get_db
 from models.users_model import User
 
 SECRET_KEY = os.getenv("SECRET_KEY", "your-super-secret-key")
-ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 30
+ALGORITHM = os.getenv("ALGORITHM", "HS256")
+ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", 30))
 
 pwd_context = CryptContext(schemes=["argon2"], deprecated="auto")
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="users/login")
